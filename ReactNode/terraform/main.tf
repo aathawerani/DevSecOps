ebe2ea62b6a5b73fdd8cc3537111f500f47b51d7
aathawerani <athawerani@gmail.com>
2025-04-08 22:45:52 +0500

    terraform

---
+++ ReactNode/terraform/main.tf
@@ -0,0 +1,216 @@
+provider "azurerm" {
+  features {}
+}
+
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+}
+
+variable "location" {
+  description = "Azure region"
+  default     = "eastus"
+}
+
+variable "prefix" {
+  description = "Prefix for resource names"
+  default     = "pgdemo"
+}
+
+variable "sql_admin_username" {
+  description = "SQL Server administrator username"
+}
+
+variable "sql_admin_password" {
+  description = "SQL Server administrator password"
+  sensitive   = true
+}
+
+variable "frontend_image" {
+  description = "Frontend container image"
+}
+
+variable "backend_image" {
+  description = "Backend container image"
+}
+
+resource "azurerm_resource_group" "demo" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Azure Container Registry
+resource "azurerm_container_registry" "acr" {
+  name                = "${var.prefix}acr"
+  resource_group_name = azurerm_resource_group.demo.name
+  location            = azurerm_resource_group.demo.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+# SQL Server
+resource "azurerm_mssql_server" "sql" {
+  name                         = "${var.prefix}-sqlserver"
+  resource_group_name          = azurerm_resource_group.demo.name
+  location                     = azurerm_resource_group.demo.location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_username
+  administrator_login_password = var.sql_admin_password
+}
+
+resource "azurerm_mssql_database" "db" {
+  name           = "paymentgateway"
+  server_id      = azurerm_mssql_server.sql.id
+  sku_name       = "Basic"
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  max_size_gb    = 2
+  zone_redundant = false
+}
+
+# AKS Cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "${var.prefix}-aks"
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
+  dns_prefix          = "${var.prefix}-aks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+# Kubernetes Deployments
+resource "kubernetes_deployment" "frontend" {
+  metadata {
+    name = "frontend"
+    labels = {
+      app = "frontend"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "frontend"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "frontend"
+        }
+      }
+
+      spec {
+        container {
+          image = var.frontend_image
+          name  = "frontend"
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "frontend" {
+  metadata {
+    name = "frontend-service"
+  }
+  spec {
+    selector = {
+      app = "frontend"
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+    type = "LoadBalancer"
+  }
+}
+
+resource "kubernetes_deployment" "backend" {
+  metadata {
+    name = "backend"
+    labels = {
+      app = "backend"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "backend"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "backend"
+        }
+      }
+
+      spec {
+        container {
+          image = var.backend_image
+          name  = "backend"
+          port {
+            container_port = 3000
+          }
+          env {
+            name  = "DB_CONNECTION_STRING"
+            value = "Server=${azurerm_mssql_server.sql.fully_qualified_domain_name};Database=${azurerm_mssql_database.db.name};User ID=${var.sql_admin_username};Password=${var.sql_admin_password};Encrypt=true;TrustServerCertificate=false;Connection Timeout=30;"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "backend" {
+  metadata {
+    name = "backend-service"
+  }
+  spec {
+    selector = {
+      app = "backend"
+    }
+    port {
+      port        = 3000
+      target_port = 3000
+    }
+    type = "ClusterIP"
+  }
+}
+
+# Outputs
+output "frontend_url" {
+  value = "http://${kubernetes_service.frontend.status.0.load_balancer.0.ingress.0.ip}"
+}
+
+output "sql_server_fqdn" {
+  value = azurerm_mssql_server.sql.fully_qualified_domain_name
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.demo.name
+}
---
+++ ReactNode/terraform/outputs.tf
@@ -0,0 +1,8 @@
+output "frontend_url" {
+  value = "http://${azurerm_public_ip.frontend.ip_address}"
+}
+
+output "sql_connection_string" {
+  value = "Server=tcp:${azurerm_mssql_server.main.fully_qualified_domain_name},1433;Database=${azurerm_mssql_database.main.name};User ID=${azurerm_mssql_server.main.administrator_login};Password=${azurerm_mssql_server.main.administrator_login_password};Encrypt=true;TrustServerCertificate=false;Connection Timeout=30;"
+  sensitive = true
+}
