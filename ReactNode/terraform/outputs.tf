272f6f16988dbd307665b2267a46d2a5181a7615
aathawerani <athawerani@gmail.com>
2025-04-26 14:53:21 +0500

    fix

--- ReactNode/terraform/outputs.tf
+++ ReactNode/terraform/outputs.tf
@@ -1,16 +1,4 @@
 # Outputs
-output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
-}
-
-output "acr_admin_username" {
-  value = azurerm_container_registry.acr.admin_username
-}
-
-output "acr_admin_password" {
-  value     = azurerm_container_registry.acr.admin_password
-  sensitive = true
-}
 
 output "frontend_url" {
   value = "http://${kubernetes_service.frontend.status.0.load_balancer.0.ingress.0.ip}"
